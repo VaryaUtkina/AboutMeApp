61a40c4ba691ca20acae9a53f2c04cfd99499dde
Varya Utkina <varya@icloud.com>
2023-11-30 21:17:16 +0300

    Right version of homework

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -13,92 +13,55 @@ final class LoginViewController: UIViewController {
     @IBOutlet var userNameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     
-    @IBOutlet var logInButton: UIButton!
-    @IBOutlet var forgotUserNameButton: UIButton!
-    @IBOutlet var forgotPasswordButton: UIButton!
-    
     // MARK: Private Properties
-    private let userName = "1"
-    private let password = "2"
+    private let user = "User"
+    private let password = "Password"
     
     // MARK: Override Methods
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.userName = user
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
-        if !userNameTextField.isEditing || !passwordTextField.isEditing {
-            self.view.endEditing(true)
-        }
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let welcomeVC = segue.destination as? WelcomeViewController
-        welcomeVC?.welcomeMessage = "Welcome, \(userNameTextField.text ?? "")!"
+        view.endEditing(true)
     }
     
     override func shouldPerformSegue(
         withIdentifier identifier: String,
         sender: Any?
     ) -> Bool {
-        guard userNameTextField.text == userName,
-              passwordTextField.text == password else {
+        guard userNameTextField.text == user, passwordTextField.text == password else {
+            showAlert(
+                title: "Invalid login or password",
+                message: "Please, enter correct login or password") {
+                    self.passwordTextField.text = ""
+                }
             return false
         }
         return true
     }
     
     // MARK: IB Actions
-    @IBAction func logInButtonTapped() {
-        guard let inputLogin = userNameTextField.text, !inputLogin.isEmpty else {
-            return
-        }
-        guard let inputPassword = passwordTextField.text, !inputPassword.isEmpty else {
-            return
-        }
-        if shouldPerformSegue(withIdentifier: "GoToWelcomeScreen", sender: Any?.self) {
-            performSegue(withIdentifier: "GoToWelcomeScreen", sender: self)
-        } else {
-            let alert = UIAlertController(
-                title: "Invalid login or password",
-                message: "Please, enter correct login and password",
-                preferredStyle: .alert
-            )
-            let okAction = UIAlertAction(title: "OK", style: .default) { _ in
-                self.passwordTextField.text = ""
-            }
-            alert.addAction(okAction)
-            present(alert, animated: true)
-        }
-    }
-    
-    @IBAction func forgotUserNameButtonTapped() {
-        let alert = UIAlertController(
-            title: "Oops!",
-            message: "Your name is 1 😉",
-            preferredStyle: .alert
-        )
-        let okAction = UIAlertAction(title: "OK", style: .default)
-        alert.addAction(okAction)
-        present(alert, animated: true)
-    }
-    
-    @IBAction func forgotPasswordButtonTapped() {
-        let alert = UIAlertController(
-            title: "Oops!",
-            message: "Your password is 2 😉",
-            preferredStyle: .alert
-        )
-        let okAction = UIAlertAction(title: "OK", style: .default)
-        alert.addAction(okAction)
-        present(alert, animated: true)
+    @IBAction func forgotRegisterData(_ sender: UIButton) {
+        sender.tag == 0 
+        ? showAlert(title: "Oops!", message: "Your name is \(user) 😉")
+        : showAlert(title: "Oops!", message: "Your password is \(password)")
     }
     
     @IBAction func unwind(for segue: UIStoryboardSegue) {
         userNameTextField.text?.removeAll()
         passwordTextField.text?.removeAll()
+    }
+    
+    private func showAlert(title: String, message: String, completion: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            completion?()
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
     
--- AboutMeApp/WelcomeViewController.swift
+++ AboutMeApp/WelcomeViewController.swift
@@ -11,31 +11,38 @@ final class WelcomeViewController: UIViewController {
 
     @IBOutlet var welcomeLabel: UILabel!
     
-    var welcomeMessage: String!
+    var userName: String!
+    
+    private let topColor = UIColor(
+        red: 222 / 255,
+        green: 144 / 255,
+        blue: 200 / 255,
+        alpha: 1
+    )
+    private let bottomColor = UIColor(
+        red: 103 / 255,
+        green: 132 / 255,
+        blue: 211 / 255,
+        alpha: 1
+    )
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        welcomeLabel.text = welcomeMessage
-        
-        let gradientLayer = CAGradientLayer()
-        let topColor = UIColor(
-            red: 222 / 255,
-            green: 144 / 255,
-            blue: 200 / 255,
-            alpha: 1
-        )
-        let bottomColor = UIColor(
-            red: 103 / 255,
-            green: 132 / 255,
-            blue: 211 / 255,
-            alpha: 1
-        )
-        
-        gradientLayer.frame = view.bounds
-        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
-        self.view.layer.insertSublayer(gradientLayer, at: 0)
-
+        view.addVerticalGradientLayer(topColor: topColor, bottomColor: bottomColor)
+        welcomeLabel.text = "Welcome, \(userName ?? "")!"
     }
 
+}
+
+// MARK: - Set background color
+extension UIView {
+    func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
+    }
 }
